7ae4135bacfa7469c5186792fbaad085ea61e745
Simon Cheam <simoncheam@Simons-MacBook-Pro.local>
2022-04-28 21:52:47 -0400

    updated 'localstorage' spelling

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
-import config, { auth0_config } from './config';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +12,7 @@ ReactDOM.render(
       domain="dev-7uyk26j2.us.auth0.com"
       clientId="ZKyDTvI7Tymi7OKJiU5VPwxi8qdQDxA7"
       redirectUri={window.location.origin}
-      cacheLocation="localStorage"
+      cacheLocation="localstorage"
     >
       <GithubProvider>
         <App />
